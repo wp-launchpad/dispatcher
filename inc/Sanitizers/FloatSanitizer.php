e725e810f67a08d6d17e17db0eea82d5a0bd3383
COQUARD Cyrille <coquardcyr@gmail.com>
2024-04-17 22:26:19 +0200

    Added docblocks

--- inc/Sanitizers/FloatSanitizer.php
+++ inc/Sanitizers/FloatSanitizer.php
@@ -8,6 +8,12 @@ use LaunchpadDispatcher\Traits\IsDefault;
 class FloatSanitizer implements SanitizerInterface {
     use IsDefault;
 
+    /**
+     * Sanitize the value.
+     *
+     * @param mixed $value Value to sanitize.
+     * @return mixed
+     */
     public function sanitize($value)
     {
         return (float) $value;
