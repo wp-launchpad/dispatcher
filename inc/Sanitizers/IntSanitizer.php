e24fcc7f5899dcda0cd57faac43f9457800beff1
COQUARD Cyrille <coquardcyr@gmail.com>
2024-04-17 23:33:42 +0200

    Added linter and fixes

--- inc/Sanitizers/IntSanitizer.php
+++ inc/Sanitizers/IntSanitizer.php
@@ -6,16 +6,15 @@ use LaunchpadDispatcher\Interfaces\SanitizerInterface;
 use LaunchpadDispatcher\Traits\IsDefault;
 
 class IntSanitizer implements SanitizerInterface {
-    use IsDefault;
+	use IsDefault;
 
-    /**
-     * Sanitize the value.
-     *
-     * @param mixed $value Value to sanitize.
-     * @return mixed
-     */
-    public function sanitize($value)
-    {
-        return (int) $value;
-    }
+	/**
+	 * Sanitize the value.
+	 *
+	 * @param mixed $value Value to sanitize.
+	 * @return mixed
+	 */
+	public function sanitize( $value ) {
+		return (int) $value;
+	}
 }
--- inc/Traits/IsDefault.php
+++ inc/Traits/IsDefault.php
@@ -2,17 +2,16 @@
 
 namespace LaunchpadDispatcher\Traits;
 
-trait IsDefault
-{
-    /**
-     * Is the value the default one.
-     *
-     * @param mixed $value Actual value.
-     * @param mixed $original Original value.
-     * @return bool
-     */
-    public function is_default($value, $original): bool
-    {
-        return $value !== $original;
-    }
+trait IsDefault {
+
+	/**
+	 * Is the value the default one.
+	 *
+	 * @param mixed $value Actual value.
+	 * @param mixed $original Original value.
+	 * @return bool
+	 */
+	public function is_default( $value, $original ): bool {
+		return $value !== $original;
+	}
 }
